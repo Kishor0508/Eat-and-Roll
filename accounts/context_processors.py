96ada8cb984cc4b5b6590535a9b7b6a9c94bdb6e
Kishor0508 <kishorkk05082001@gmail.com>
2024-04-17 21:46:08 +0530

    first commit

---
+++ accounts/context_processors.py
@@ -0,0 +1,28 @@
+from .models import userProfile
+from vendor.models import Vendor
+from django.conf import settings
+
+
+def get_vendor(request):
+    try:
+        vendor = Vendor.objects.get(user=request.user)
+    except:
+        vendor = None
+    return dict(vendor=vendor)
+
+
+def get_user_profile(request):
+    try:
+        user_profile = userProfile.objects.get(user=request.user)
+    except:
+        print("none")
+        user_profile = None
+    return dict(user_profile=user_profile)
+
+
+def get_google_api(request):
+    return {"GOOGLE_API_KEY": settings.GOOGLE_API_KEY}
+
+
+def get_paypal_client_id(request):
+    return {"PAYPAL_CLIENT_ID": settings.PAYPAL_CLIENT_ID}
